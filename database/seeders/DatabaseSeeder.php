94d653d6b8a5e2c868c6bca21ebc9193797a9585
arynpet <muhammad.aryan.v.p@gmail.com>
2025-11-23 06:24:32 +0700

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
-        // Catatan: Anda bisa menambahkan admin lain sesuai kebutuhan di sini.
+        $this->call([
+            AdminSeeder::class,
+        ]);
     }
 }
